ea5b7b58b1ce8a690f6926779e9c92a18a651ae6
Renato Groffe <renato.groffe@yahoo.com.br>
2021-07-07 17:09:43 -0300

    .NET 6 Preview 5 + ASP.NET Core + Razor Pages + Compile-time logging source generation + Docker + Linux
    .NET 6 Preview 5 + ASP.NET Core + Razor Pages + Compile-time logging source generation + Docker + Linux

---
+++ SiteContagem/Logging/ContagemLogging.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Logging;
+
+namespace SiteContagem.Logging
+{
+    public static partial class ContagemLogging
+    {
+        [LoggerMessage(EventId = 1, Level = LogLevel.Information,
+            Message = "Contador - Valor atual: {valorAtual}")]
+        public static partial void LogValorAtual(
+            this ILogger logger, int valorAtual);        
+    }
+}
---
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SiteContagem.Logging;
+
+namespace SiteContagem.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private static readonly Contador _CONTADOR = new();
+
+        public void OnGet([FromServices] ILogger<IndexModel> logger,
+            [FromServices] IConfiguration configuration)
+        {
+            int valorAtual;
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+                valorAtual = _CONTADOR.ValorAtual;
+            }
+
+            logger.LogValorAtual(valorAtual);
+
+            TempData["Contador"] = valorAtual;
+            TempData["Local"] = _CONTADOR.Local;
+            TempData["Kernel"] = _CONTADOR.Kernel;
+            TempData["TargetFramework"] = _CONTADOR.TargetFramework;
+            TempData["MensagemFixa"] = "Teste";
+            TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
+        }
+    }
+}
